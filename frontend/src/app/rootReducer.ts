6f74585ac73baa2713e83bff1d3e9a0b50b5086c
Timofey Pletnev <37830040+timatifey@users.noreply.github.com>
2022-11-30 16:58:41 +0300

    Revert "Issue #13: Добавление базовой разметки"
    This reverts commit 240d5028903705cae733b78754688d10716882ea.

--- frontend/src/app/rootReducer.ts
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "@reduxjs/toolkit";
-import { StageReducer } from "./stage/stageSlise";
-
-export const rootReducer = combineReducers({
-    stage: StageReducer,
-});
-
-export type RootState = ReturnType<typeof rootReducer>;
--- frontend/src/components/header/index.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "./header.module.scss";
-
-function Header() {
-    return (
-        <div className={styles.logo}>YouGIF</div>
-    );
-}
-
-export default Header;
