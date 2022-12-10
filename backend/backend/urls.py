ae3e787258e7697ba23a871cbfa33f993be7edd8
Плетнев Тимофей Сергеевич <pletnev.ts@edu.spbstu.ru>
2022-12-10 23:50:49 +0300

    #11: sending gif into file response

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -13,8 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path, include
+from django.urls import path
+import yougif.views
 
 urlpatterns = [
-    path('download/', include('yougif.urls')),
+    path('convert/', yougif.views.convert_youtube_video),
 ]
