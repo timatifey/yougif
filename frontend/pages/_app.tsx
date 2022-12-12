8f315feeba005a0263a83eb838913497fa7a0b88
Denis Zhidkov <denis.zhidkov01@gmail.com>
2022-12-12 18:49:33 +0300

    #5: Добавление тёмной темы

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,12 +1,15 @@
-import '../styles/index.scss'
+import '../styles/globals.scss'
 import type {AppProps} from 'next/app'
 import {Provider} from "react-redux";
 import {store} from "../src/app/store";
+import {ThemeProvider} from "next-themes";
 
 export default function App({Component, pageProps}: AppProps) {
     return (
         <Provider store={store}>
-            <Component {...pageProps} />
+            <ThemeProvider forcedTheme={Component.theme || undefined}>
+                <Component {...pageProps} />
+            </ThemeProvider>
         </Provider>
     );
 }
--- frontend/src/components/header/index.tsx
+++ frontend/src/components/header/index.tsx
@@ -1,8 +1,12 @@
 import styles from "./header.module.scss";
+import ThemeChanger from "../theme-changer";
 
 function Header() {
     return (
-        <div className={styles.logo}>YouGIF</div>
+        <div className={styles.header_container}>
+            <div className={styles.header_container__logo}>YouGIF</div>
+            <ThemeChanger />
+        </div>
     );
 }
 
